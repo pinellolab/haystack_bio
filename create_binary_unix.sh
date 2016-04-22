844f204d60c889e32eb9ae222ab9b19d839c7fb7
Luca Pinello <lpinello@jimmy.harvard.edu>
2016-04-22 14:56:18 -0400

    First cleanup for the transition

--- create_binary_unix.sh
+++
@@ -1,8 +0,0 @@
-rm precompiled_binary/Linux/haystack*
-rm precompiled_binary/Linux/download_genome
-pyinstaller haystack_pipeline.spec --clean --distpath  precompiled_binary/Linux/
-pyinstaller download_genome.spec --clean --distpath precompiled_binary/Linux/
-pyinstaller haystack_hotspots.unix.spec --clean --distpath  precompiled_binary/Linux/
-pyinstaller haystack_motifs.spec --clean --distpath  precompiled_binary/Linux/
-pyinstaller haystack_tf_activity_plane.unix.spec --clean --distpath  precompiled_binary/Linux/
-rm -Rf build
