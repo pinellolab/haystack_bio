724486ff1891bf6dc8e72b3a99e7a694d6565a2b
Rick Farouni <rfarouni@gmail.com>
2017-07-07 15:53:46 -0400

    removed redundant Genome_2bit class declaration in download genome

--- haystack/haystack_download_genome_CORE.py
+++ haystack/haystack_download_genome_CORE.py
@@ -1,60 +1,9 @@
 import os
 import sys
 import  urllib2
-import shutil as sh 
-from bx.seq.twobit import TwoBitFile
-import time
-
-
+import shutil as sh
+from bioutilities import Genome_2bit
 from haystack_common import determine_path, HAYSTACK_VERSION
-
-class Genome_2bit:
-
-    def __init__(self,genome_2bit_file,verbose=False):
-        self.genome=TwoBitFile(open(genome_2bit_file,'rb'))
-        self.chr_len=dict()
-        self.verbose=verbose
-
-        for chr_id in self.genome.keys():
-            self.chr_len[chr_id]=len(self.genome[chr_id])
-        if verbose:
-            print 'Genome initializated'
-        
-   
-    def estimate_background(self):
-        counting={'a':.0,'c':.0,'g':.0,'t':.0}
-        all=0.0
-        
-        for chr_id in self.genome.keys():
-            if self.verbose:
-                start_time = time.time()
-                print 'Counting on:',chr_id
-
-            for nt in counting.keys():
-                
-                count_nt=self.genome[chr_id][:].lower().count(nt)
-                counting[nt]+=count_nt
-                all+=count_nt
-        
-        if self.verbose:
-            print counting
-
-        for nt in counting.keys():
-            counting[nt]/=all
-        
-        return counting
-
-
-    def write_meme_background(self,filename):
-        counting=self.estimate_background()
-        with open(filename,'w+') as outfile:
-            for nt in counting.keys():
-                outfile.write('%s\t%2.4f\n' % (nt.upper(),counting[nt]))
-
-    def write_chr_len(self,filename):
-        with open(filename,'w+') as outfile:
-            for chr_id in self.genome.keys():
-                outfile.write('%s\t%s\n' % (chr_id,self.chr_len[chr_id]) )
 
 
 def download_genome(name, output_directory=None):
