c6bd3404931d0abf10e5db2d87c30844b798a77e
Luca Pinello <lucapinello@gmail.com>
2017-09-30 09:46:30 -0400

    docker test scripts for bam and bigwig

--- scripts/docker_test_bam.sh
+++ scripts/docker_test_bam.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash 
-docker pull pinellolab/haystack_bio
 mkdir ${HOME}/haystack_genomes
-wget https://www.dropbox.com/s/4yjx7ypj0c82ryh/data_h3k27ac_6cells.zip?dl=1
-unzip data_h3k27ac_6cells.zip?dl=1 -d $HOME
+wget -O data_h3k27ac_6cells.zip https://www.dropbox.com/s/4yjx7ypj0c82ryh/data_h3k27ac_6cells.zip?dl=1
+unzip data_h3k27ac_6cells.zip -d $HOME
+rm data_h3k27ac_6cells.zip
 cd $HOME/data_h3k27ac_6cells
+rm -Rf HAYSTACK_PIPELINE_RESULTS
 
+docker pull pinellolab/haystack_bio
 docker run -v ${PWD}:/docker_data \
            -v ${HOME}/haystack_genomes:/haystack_genomes \
            -w /docker_data -it pinellolab/haystack_bio haystack_pipeline samples_names.txt hg19  --blacklist hg19
--- scripts/docker_test_bigwig.sh
+++ scripts/docker_test_bigwig.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash 
-docker pull pinellolab/haystack_bio
 mkdir ${HOME}/haystack_genomes
-wget https://www.dropbox.com/s/puz6azpn5z51lm6/bigwigs_h3k27ac_6cells.zip?dl=1
-unzip bigwigs_h3k27ac_6cells.zip?dl=1 -d $HOME
+wget -O bigwigs_h3k27ac_6cells.zip  https://www.dropbox.com/s/puz6azpn5z51lm6/bigwigs_h3k27ac_6cells.zip?dl=1
+unzip bigwigs_h3k27ac_6cells.zip -d $HOME
+rm bigwigs_h3k27ac_6cells.zip
 cd $HOME/bigwigs_h3k27ac_6cells
+rm -Rf HAYSTACK_PIPELINE_RESULTS
 
+docker pull pinellolab/haystack_bio
 docker run -v ${PWD}:/docker_data \
            -v ${HOME}/haystack_genomes:/haystack_genomes \
-           -w /docker_data -it pinellolab/haystack_bio haystack_pipeline samples_names.txt hg19  --blacklist hg19 --input_is_bigwig
-
+           -w /docker_data -it pinellolab/haystack_bio haystack_pipeline sample_names.txt hg19  --blacklist hg19 --input_is_bigwig
 
